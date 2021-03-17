11608beff364d6c2f798ddd8ecd6c52841b42706
Aman Sanjiv Patel <patelas@sh03-12n03.int>
2021-03-17 08:57:53 -0700

    Updated sim funcs

--- hyperLAI/utils/sim_funcs.py
+++ hyperLAI/utils/sim_funcs.py
@@ -2,7 +2,13 @@ import numpy as np
 import torch
 from sklearn.metrics import adjusted_mutual_info_score
 
+def mnist_label_evenodd(x1,x2):
+    same_label = int(x1 == x2)
+    same_mod = int((x1 % 2) == (x2 % 2))
+    return same_label + same_mod
+
 sim_func_dict = {
     "hamming": lambda x,y: torch.true_divide(torch.sum(x==y), len(x)),
-    "minf": adjusted_mutual_info_score
+    "minf": adjusted_mutual_info_score,
+    "mnist_even_odd": mnist_label_evenodd
 }
